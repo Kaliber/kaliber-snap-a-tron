29be3d0b26900090db80cf73c6d807e61e2520bc
Leroy Davidson Descelles <ldavidsondescelles@kaliber.net>
2023-09-06 12:45:07 +0200

    String update

--- src/scenes/ConfirmationStart.js
+++ src/scenes/ConfirmationStart.js
@@ -2,7 +2,7 @@ import { AnimatedString } from '/components/effects/AnimatedString'
 import styles from './ConfirmationStart.css'
 
 export function ConfirmationStart() {
-  const introText = 'Welcome traveler. Venture forth into a digital adventure. Where relics of the past hold secrets untold.'
+  const introText = 'Welcome traveler. Venture forth into a digital adventure. Where relics of the past hold secrets untold. Follow the instructions closely and connect the past with the future to create your own AI portrait. Are you ready?'
 
   return (
     <div className={styles.component}>
